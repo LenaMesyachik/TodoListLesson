fcbe2ed9e89dfd550b7e9f9a66db59407f6438e3
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-03 12:53:34 +0300

    create a parent component <App/>

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,25 @@ import './App.css';
 
 export const  App = () => {
     return (
-        <div>
+        <div className='App'>
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/><button> + </button>
+                </div>
+               <ul>
+                    <li> <input type = 'checkbox' checked = {true}/> <span> HTML&CSS</span></li>
+                    <li> <input type = 'checkbox' checked = {false}/> <span> js</span></li>
+                    <li> <input type = 'checkbox' checked = {true}/> <span> REACT</span></li>
+               </ul>
+                <div>
+                    <button>ALL</button>
+                    <button>ACTIVE</button>
+                    <button>COMPLETED</button>
+                </div>
+            </div>
 
-            hello world
+
     </div>
     )}
 
