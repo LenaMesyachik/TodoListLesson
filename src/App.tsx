44b84606a58eb39a6d47ed370a6a146ede8270c0
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-03 13:01:49 +0300

    move the markup to the child component <TodoList/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from "./TodoList";
 
 
 export const  App = () => {
     return (
         <div className='App'>
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/><button> + </button>
-                </div>
-               <ul>
-                    <li> <input type = 'checkbox' checked = {true}/> <span> HTML&CSS</span></li>
-                    <li> <input type = 'checkbox' checked = {false}/> <span> js</span></li>
-                    <li> <input type = 'checkbox' checked = {true}/> <span> REACT</span></li>
-               </ul>
-                <div>
-                    <button>ALL</button>
-                    <button>ACTIVE</button>
-                    <button>COMPLETED</button>
-                </div>
-            </div>
-
-
-    </div>
+        <TodoList/>
+        </div>
     )}
 
-export default App;
+export default App
---
+++ src/TodoList.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+export const  TodoList = () => {
+    return (
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/><button> + </button>
+                </div>
+               <ul>
+                    <li> <input type = 'checkbox' checked = {true}/> <span> HTML&CSS</span></li>
+                    <li> <input type = 'checkbox' checked = {false}/> <span> js</span></li>
+                    <li> <input type = 'checkbox' checked = {true}/> <span> REACT</span></li>
+               </ul>
+                <div>
+                    <button>ALL</button>
+                    <button>ACTIVE</button>
+                    <button>COMPLETED</button>
+                </div>
+            </div>
+    )}
+
