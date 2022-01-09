5ae7c63424b70df62454c9de3eb0a11668322119
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-09 16:31:20 +0300

    lesson 10 create store

---
+++ src/state/store.ts
@@ -0,0 +1,15 @@
+import {combineReducers, createStore} from "redux";
+import {taskReducer} from "./TaskReducer";
+import {todoListReducer} from "./TodoListReducer";
+
+
+const rootReducer = combineReducers({
+    task: taskReducer,
+    todoList: todoListReducer})
+
+export const store = createStore(rootReducer)
+export type rootStateType = ReturnType<typeof  rootReducer >
+
+
+// @ts-ignore
+window.store = store
